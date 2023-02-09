89accb7fe22f8dd1c58638ab1ea644839d1b2c59
Syrym <himces@gmail.com>
2023-02-09 09:26:57 +0600

    feat new redis template add

--- redisTemplate.go
+++ redisTemplate.go
@@ -6,7 +6,7 @@ import (
 )
 
 type Redis struct {
-	Client *redis.Client
+	Client redis.Client
 }
 
 func NewRedisClient(redisURL string, password string, database int) *redis.Client {
@@ -18,32 +18,32 @@ func NewRedisClient(redisURL string, password string, database int) *redis.Clien
 }
 
 // Ping tests a connection to the Redis server.
-func (c *Redis) Ping() (string, error) {
+func (c Redis) Ping() (string, error) {
 	return c.Client.Ping().Result()
 }
 
 // Get gets the value of a key.
-func (c *Redis) Get(key string) (string, error) {
+func (c Redis) Get(key string) (string, error) {
 	return c.Client.Get(key).Result()
 }
 
 // Set sets the value of a key.
-func (c *Redis) Set(key string, value string) error {
+func (c Redis) Set(key string, value string) error {
 	return c.Client.Set(key, value, 0).Err()
 }
 
 // Del deletes one or more keys.
-func (c *Redis) Del(keys ...string) error {
+func (c Redis) Del(keys ...string) error {
 	return c.Client.Del(keys...).Err()
 }
 
 // Exists determines if a key exists.
-func (c *Redis) Exists(key string) (int64, error) {
+func (c Redis) Exists(key string) (int64, error) {
 	return c.Client.Exists(key).Result()
 }
 
 // Expire sets a timeout on a key.
-func (c *Redis) Expire(key string, duration int64) error {
+func (c Redis) Expire(key string, duration int64) error {
 	return c.Client.Expire(key, time.Duration(duration)).Err()
 }
 
@@ -53,21 +53,21 @@ func (c *Redis) TTL(key string) (time.Duration, error) {
 }
 
 // Incr increments the value of a key.
-func (c *Redis) Incr(key string) (int64, error) {
+func (c Redis) Incr(key string) (int64, error) {
 	return c.Client.Incr(key).Result()
 }
 
 // Decr decrements the value of a key.
-func (c *Redis) Decr(key string) (int64, error) {
+func (c Redis) Decr(key string) (int64, error) {
 	return c.Client.Decr(key).Result()
 }
 
 // HGet gets the value of a field in a hash.
-func (c *Redis) HGet(key string, field string) (string, error) {
+func (c Redis) HGet(key string, field string) (string, error) {
 	return c.Client.HGet(key, field).Result()
 }
 
 // HSet sets the value of a field in a hash.
-func (c *Redis) HSet(key string, field string, value string) error {
+func (c Redis) HSet(key string, field string, value string) error {
 	return c.Client.HSet(key, field, value).Err()
 }
